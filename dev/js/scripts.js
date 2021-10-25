6de70f38300481a8489665b0e1841c6b5450ef18
eco38drexel <81782897+eco38drexel@users.noreply.github.com>
2021-10-25 00:32:03 -0400

    dev tools set up properly

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,7 @@
 import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(GSDevTools);
 
 const mainTL = gsap.timeline();
 
@@ -14,3 +17,6 @@ gsap.registerEffect({
 })
 
 gsap.effects.loop("#be")
+
+
+GSDevTools.create();
