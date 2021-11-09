2a9e6bac815047f129736b97328f3c4370e18c26
eco38drexel <81782897+eco38drexel@users.noreply.github.com>
2021-11-09 15:20:45 -0500

    preloader most recent nov 9

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,30 +2,49 @@ import { gsap } from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
-
-gsap.registerPlugin(DrawSVGPlugin);
-
-gsap.registerPlugin(GSDevTools);
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#sneakers",{duration:1.5,y:"-=300"})
-.from("#hero",{duration:2,alpha:0},"-=1.5")
-.from("#hero-content h1",{duration:2,alpha:0});
+function preloaderAni(){
+    const tl = gsap.timeline();
 
-gsap.registerEffect({
-    name:"loop",
-    effect: (targets) => {
-        return gsap.from(targets, {duration:10, x:"-=300", repeat:10, ease:"none"})
-    }
-})
+    var Preloader = ["#pants-complete", "#sweatshirt-complete", "#shoe-complete"];
+    tl.from("#shoe-complete",{drawSVG: "0%", duration: 5})
+    .to("#shoe",{duration: 1, alpha:0},"sameTime")
+    .from("#sweatshirt-complete",{duration: 5, drawSVG: "0%"})
+    .to("#sweatshirt",{delay: 5, duration: 1, alpha:0},"sameTime")
+    .from("#pants-complete",{duration: 5, drawSVG: "0%"});
+    // tl.fromTo(Preloader, {drawSVG:"0% 100%"}, {duration: 1, drawSVG:"100%", stagger: 2})
+    return tl;
+}
 
-gsap.effects.loop("#be")
+function heroAnimation(){
+    const tl = gsap.timeline();
+    tl.from("#sneakers",{duration:1.5,y:"-=300"})
+    .from("#hero",{duration:2,alpha:0},"-=1.5")
+    .from("#hero-content h1",{duration:2,alpha:0});
+    return tl;
+}
+
+function bannerAnimation(){
+    const tl = gsap.timeline({repeat:10});
+    tl.from("#be", {duration:10, x:"-=300", repeat:10, ease:"none"})
+    return tl;
+}
+
+mainTL.add(preloaderAni());
+
+
+
+// gsap.registerEffect({
+//     name:"loop",
+//     effect: (targets) => {
+//         return gsap.from(targets, {duration:10, x:"-=300", repeat:10, ease:"none"})
+//     }
+// })
+
+// gsap.effects.loop("#be")
 
 
 GSDevTools.create();
-
-var Preloader = "pants-complete, sweatshirt-complete, shoe-complete",
-    tl = gsap.timeline({repeat:1});
-
-tl.fromTo(Preloader, {drawSVG:"100%"}, {duration: 1, drawSVG:"100%", stagger: 2})
